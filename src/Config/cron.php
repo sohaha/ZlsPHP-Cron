d3cf7ffe3bb76827b7c5e6489d9c68a37c4fc707
seekwe <soseek@vip.qq.com>
2019-01-11 17:42:02 +0800

    first commit

---
+++ src/Config/cron.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * Zls\Cron\Config
+ * @author        影浅
+ * @email         seekwe@gmail.com
+ * @copyright     Copyright (c) 2019, 影浅, Inc.
+ * @updatetime    2019-01-11 12:39
+ */
+return [
+    'enable' => true,
+    'lists'  => [
+        //[
+        //'task'    => '',
+        //'enable'  => true,
+        //'args'    => '',
+        //'cron'    => ' */1 * * * *',
+        //'logPath' => '',
+        //],
+    ],
+];
